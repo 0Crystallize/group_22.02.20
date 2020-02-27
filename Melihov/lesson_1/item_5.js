3d8768bed17f7df8bdba3947bff0a1728e08b545
Maskati <melikhov-maksim@rambler.ru>
2020-02-27 22:05:00 +0300

    add gitignore file

---
+++ Melihov/lesson_1/item_5.js
@@ -0,0 +1,23 @@
+'use strict';
+
+//Реализовать основные 4 арифметические операции (+, -, /, *) в виде функций с двумя параметрами.
+// Обязательно использовать оператор return.
+
+let a = 2;
+let b = 3;
+
+function plus(a, b) {
+    return a + b;
+}
+
+function minus(a, b) {
+    return a - b;
+}
+
+function div (a, b) {
+    return a / b;
+}
+
+function mult (a, b) {
+    return a * b;
+}
