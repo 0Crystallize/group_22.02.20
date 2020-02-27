d10bda7933fa78cf251b4c6049fdd382c99105b8
indexdz <ekaterina6277@gmail.com>
2020-02-28 01:12:21 +0300

    ДЗ1

---
+++ biserova/lesson_1/item_5/index_5.js
@@ -0,0 +1,23 @@
+function add (a, b) {
+    return a + b;
+}
+console.log(5 + 4)
+
+
+function minus (a, b) {
+    return a- b;
+}
+
+console.log(5 - 4)
+
+function add (a, b) {
+    return a * b;
+}
+
+console.log(5 * 4)
+
+function add (a, b) {
+    return a / b;
+}
+
+console.log(5 / 4)
